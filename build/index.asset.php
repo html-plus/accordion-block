67355aecdd697495fb84b5f7914275c6b0fc8a14
html-plus <html.plus.in.ua@gmail.com>
2024-08-04 19:44:40 +0300

    update styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9e3b7b11d9d73d988348');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '234561490982935ea171');
